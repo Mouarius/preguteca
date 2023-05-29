5442f6fa1360614ed726824ef5939a7951ae7710
Marius Menault <mouarius@gmail.com>
2023-05-30 00:05:32 +0200

    connected frontend to backend, base category views

--- preguteca_frontend/src/App.vue
+++ preguteca_frontend/src/App.vue
@@ -1,147 +1,125 @@
 <script setup lang="ts">
-import VideoEntry from "./components/VideoEntry.vue";
+import {reactive} from "vue";
+import CategoryDetail from "./components/CategoryDetail.vue";
+import useCategories from "./queries/useCategories.ts";
+import {TCategory} from "./types";
+
+
+const globalState = reactive({
+  activeCategory: {} as TCategory
+})
+const {isLoading, isError, categories, error} = useCategories()
+
+function handleCategoryClick(category) {
+  globalState.activeCategory = category
+
+}
+
+
 </script>
 
 <template>
 
-    <header id="page-header" class="border-thin">
-        <h1 id="page-title">PREGUTECA</h1>
-    </header>
-    <div id="page-content" class="border-thin">
-        <section id="main-scroll">
-            <div id="scroll-illustration">
-
-            </div>
-
-        </section>
-        <section id="category-container">
-            <header>
-                <h2>LA ESCUELA</h2>
-            </header>
-            <ul id="video-entry-list">
-
-                <VideoEntry/>
-                <VideoEntry/>
-                <VideoEntry/>
-                <VideoEntry/>
-            </ul>
-        </section>
-    </div>
+  <header id="page-header" class="border-thin">
+    <h1 id="page-title">PREGUTECA</h1>
+  </header>
+  <div id="page-content" class="border-thin">
+    <section id="main-scroll">
+      <div id="scroll-illustration">
+        <div v-if="isLoading">Loading...</div>
+        <div v-else-if="isError">{{ error.message }}</div>
+        <ul v-else-if="categories">
+          <li v-for="category in categories">
+            <button v-on:click="handleCategoryClick(category)" :value="category.name">
+              {{ category.full_name }}
+            </button>
+          </li>
+        </ul>
+      </div>
+    </section>
+    <CategoryDetail :active-category="globalState.activeCategory"/>
+  </div>
 </template>
 
 <style>
 #app {
-    max-height: 100vh;
-    width: 100vw;
-    left: 0;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    padding: 8px;
-    display: grid;
-    grid-template-columns: 1fr;
-    column-gap: 8px;
-    row-gap: 8px;
-    grid-template-rows: 52px minmax(0, 1fr);
-    grid-template-areas: "header" "content";
-    overflow: hidden;
+  height: 100vh;
+  width: 100vw;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  padding: 8px;
+  display: grid;
+  grid-template-columns: 1fr;
+  column-gap: 8px;
+  row-gap: 8px;
+  grid-template-rows: 52px minmax(0, 1fr);
+  grid-template-areas: "header" "content";
+  overflow: hidden;
 }
 
 #page-header {
-    position: relative;
-    top: 0;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    padding-left: 16px;
-    grid-area: header;
+  position: relative;
+  top: 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding-left: 16px;
+  grid-area: header;
 }
 
 #page-title {
-    font-size: 32px;
-    font-weight: 500;
+  font-size: 32px;
+  font-weight: 500;
 }
 
 #page-content {
-    position: relative;
-    box-sizing: border-box;
-    grid-area: content;
-    display: grid;
-    bottom: 0;
-    grid-template-columns: repeat(5, 1fr);
-    grid-template-rows: minmax(0, 1fr);
-    grid-template-areas: "main-scroll main-scroll aside aside aside";
+  position: relative;
+  box-sizing: border-box;
+  grid-area: content;
+  display: grid;
+  bottom: 0;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: minmax(0, 1fr);
+  grid-template-areas: "main-scroll main-scroll aside aside aside";
 }
 
 #main-scroll {
-    grid-area: main-scroll;
-}
-
-
-#category-container {
-    grid-area: aside;
-    border-left: solid 1px var(--border-color);
-    position: relative;
-    display: flex;
-    flex-direction: column;
-}
-
-#category-container > header {
-    background: var(--white);
-    padding-left: 16px;
-    height: 42px;
-    width: 100%;
-    color: var(--text);
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-}
-
-
-#video-entry-list {
-    padding: 8px;
-    display: flex;
-    flex-direction: column;
-    align-self: stretch;
-    align-items: center;
-    justify-content: flex-start;
-    width: 100%;
-    height: 100%;
-    position: relative;
-    overflow: scroll;
+  grid-area: main-scroll;
 }
 
 
 .border-thin {
-    border: solid 1px var(--border-color)
+  border: solid 1px var(--border-color)
 }
 
 
 h1 {
-    font-family: "Space Grotesk", "Arial Black", sans-serif;
-    font-size: 2.4rem;
-    font-weight: 600;
+  font-family: "Space Grotesk", "Arial Black", sans-serif;
+  font-size: 2.4rem;
+  font-weight: 600;
 }
 
 h2 {
-    font-family: "Space Grotesk", "Arial Black", sans-serif;
-    font-size: 24px;
-    font-weight: 600;
+  font-family: "Space Grotesk", "Arial Black", sans-serif;
+  font-size: 24px;
+  font-weight: 600;
 }
 
 h3 {
-    font-size: 1.4rem;
-    font-weight: 600;
+  font-size: 1.4rem;
+  font-weight: 600;
 }
 
 li {
-    list-style-type: none;
+  list-style-type: none;
 }
 
 
 .container {
-    display: block;
-    position: relative;
-    width: 100%;
+  display: block;
+  position: relative;
+  width: 100%;
 }
 </style>

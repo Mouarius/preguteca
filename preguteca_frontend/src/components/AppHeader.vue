de40fa8a3e7ec0896994beacdb051555aebd77d4
Marius Menault <marius.me@hellowatt.fr>
2023-07-03 11:06:08 +0200

    changes from the train

---
+++ preguteca_frontend/src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header>
+    <h1 id="page-title">PREGUTECA</h1>
+  </header>
+</template>
+
+<style scoped>
+header {
+  position: relative;
+  top: 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding-left: 16px;
+  grid-area: header;
+  border: solid 1px var(--border-color)
+}
+
+#page-title {
+  font-size: 32px;
+  font-weight: 500;
+}
+
+</style>

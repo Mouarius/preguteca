8fd73319f105597bd8b7704bd1ce4d4367cc0eeb
Marius Menault <mouarius@gmail.com>
2023-09-30 16:26:32 +0200

    remove base_url

--- preguteca_frontend/src/main.ts
+++ preguteca_frontend/src/main.ts
@@ -2,11 +2,10 @@ import { createApp } from "vue";
 import { VueQueryPlugin } from "@tanstack/vue-query";
 import App from "./App.vue";
 import axios from "axios";
-import { BASE_URL } from "./settings";
 
 const app = createApp(App);
 export const axiosInstance = axios.create({
-  baseURL: `${BASE_URL}api/`,
+  baseURL: `api/`,
   headers: {
     "Content-Type": "application/json",
   },
--- preguteca_frontend/src/settings.ts
+++
@@ -1,4 +0,0 @@
-export const BASE_URL =
-  process.env.NODE_ENV === "development"
-    ? "http://127.0.0.1:8000/"
-    : "http://127.0.0.1:8000/";

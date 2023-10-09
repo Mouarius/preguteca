1bc5cded0df16c1103c7d1f902d75147d3e26dcf
Marius Menault <mouarius@gmail.com>
2023-10-09 14:57:58 +0200

    fix config api url

--- preguteca_frontend/src/main.ts
+++ preguteca_frontend/src/main.ts
@@ -2,10 +2,11 @@ import { createApp } from "vue";
 import { VueQueryPlugin } from "@tanstack/vue-query";
 import App from "./App.vue";
 import axios from "axios";
+import { API_BASE_URL } from "./config";
 
 const app = createApp(App);
 export const axiosInstance = axios.create({
-  baseURL: `api/`,
+  baseURL: `${API_BASE_URL}api/`,
   headers: {
     "Content-Type": "application/json",
   },

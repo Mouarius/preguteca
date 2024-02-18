e0e15e7eafd53d53a5ef7b8791f7c327f37e247c
Marius Menault <mouarius@gmail.com>
2024-02-18 17:19:32 +0100

    fix searchbar add fuzzy finding

--- preguteca_frontend/src/components/SearchBar.vue
+++ preguteca_frontend/src/components/SearchBar.vue
@@ -3,9 +3,9 @@ import { Ref, computed, reactive, ref } from "vue";
 import SearchIcon from "../assets/search-icon.svg";
 import { useCategories } from "../queries/useCategories";
 import { TCategory } from "../types";
-import { updateActiveCategory } from "../store";
+import { updateActiveCategory, updateActivePanel } from "../store";
+import Fuse from "fuse.js"
 
-// const CHOICES_DISPLAY_COUNT = 10
 const MIN_CHARACTERS_TO_START = 3;
 
 let searchText = ref("");
@@ -21,15 +21,20 @@ const matchedCategories: Ref<TCategory[]> = computed(() => {
   }
   return [];
 });
-const matchedCategoryElements = ref<HTMLLIElement[]>([]);
+
+const setActiveChoice = (key: number, category: TCategory) => {
+  activeChoice.key = key
+  activeChoice.category = category
+}
 
 function getMatchedCategories(
   searchText: string,
   categoriesList: TCategory[]
 ): TCategory[] {
-  return categoriesList.filter((cat) =>
-    cat.fullName.toLocaleLowerCase().startsWith(searchText)
-  );
+  const fuse = new Fuse(categoriesList, { keys: ["fullName", "name"] })
+  const result = fuse.search(searchText)
+  console.log(result)
+  return result.map(res => res.item)
 }
 
 function handleFormSubmit(event: Event) {
@@ -44,6 +49,7 @@ function handleFormSubmit(event: Event) {
     if (!activeChoice.category) {
       activeChoice.category = matchedCategories[0];
     }
+    updateActivePanel("category")
     updateActiveCategory(activeChoice.category);
     clearSearchBar();
   }
@@ -54,35 +60,11 @@ function clearSearchBar() {
   searchText.value = "";
 }
 
-function getSelectedCategory(categoryElements: HTMLLIElement[]) {
-  const categoryElement = categoryElements.find((el) => {
-    const key = el.getAttribute("data-key");
-    if (key) {
-      return parseInt(key, 10) === activeChoice.key;
-    }
-    return false;
-  });
-  if (!categoryElement) return;
-  const categoryId = categoryElement.dataset.categoryId;
-  if (!categoryId) return;
-  return categories.data?.find((cat) => cat.id === parseInt(categoryId, 10));
-}
-
-function handleSuggestionHover(event: MouseEvent) {
-  event.preventDefault();
-  if (!event.target) return;
-  activeChoice.key =
-    parseInt((event.target as HTMLElement).dataset.key ?? "0", 10) ?? 0;
-  activeChoice.category = getSelectedCategory(matchedCategoryElements.value);
-}
-
-function handleSuggestionClick(event: MouseEvent) {
-  event.preventDefault();
-  if (!event.target) return;
-  activeChoice.key =
-    parseInt((event.target as HTMLElement).dataset.key ?? "0", 10) ?? 0;
-  activeChoice.category = getSelectedCategory(matchedCategoryElements.value);
+function handleSuggestionClick(index: number, category: TCategory) {
+  activeChoice.key = index
+  activeChoice.category = category;
   if (!activeChoice.category || !activeChoice.key) return;
+  updateActivePanel("category")
   updateActiveCategory(activeChoice.category);
   clearSearchBar();
 }
@@ -92,17 +74,13 @@ function handleKeyDown(event: KeyboardEvent) {
     case "ArrowDown":
       if (activeChoice.key <= 8) {
         activeChoice.key += 1;
-        activeChoice.category = getSelectedCategory(
-          matchedCategoryElements.value
-        );
+        activeChoice.category = matchedCategories.value[activeChoice.key]
       }
       break;
     case "ArrowUp":
       if (activeChoice.key >= 1) {
         activeChoice.key -= 1;
-        activeChoice.category = getSelectedCategory(
-          matchedCategoryElements.value
-        );
+        activeChoice.category = matchedCategories.value[activeChoice.key]
       }
       break;
 
@@ -113,40 +91,18 @@ function handleKeyDown(event: KeyboardEvent) {
 </script>
 <template>
   <div class="search-bar">
-    <form @submit="handleFormSubmit" action="get">
-      <input
-        id="search-bar__autocomplete"
-        autocomplete="off"
-        dir="ltr"
-        spellcheck="false"
-        autocorrect="off"
-        autocapitalize="off"
-        tabindex="1"
-        v-model="searchText"
-        type="text"
-        :onkeydown="handleKeyDown"
-      />
+    <form action="get" @submit="handleFormSubmit">
+      <input id="search-bar__autocomplete" v-model="searchText" autocomplete="off" dir="ltr" spellcheck="false"
+        autocorrect="off" autocapitalize="off" tabindex="1" type="text" :onkeydown="handleKeyDown" />
       <button type="submit"><img :src="SearchIcon" /></button>
     </form>
-    <div
-      v-if="
-        matchedCategories.length > 0 &&
-        searchText.length >= MIN_CHARACTERS_TO_START
-      "
-      class="suggestions"
-    >
+    <div v-if="matchedCategories.length > 0 &&
+      searchText.length >= MIN_CHARACTERS_TO_START
+      " class="suggestions">
       <ul>
-        <li
-          v-for="(category, index) in matchedCategories"
-          :class="{ active: index === activeChoice.key }"
-          :key="category.id"
-          :data-key="index"
-          ref="matchedCategoryElements"
-          :data-category-id="category.id"
-          class="suggestion__element"
-          @mouseenter="handleSuggestionHover"
-          @click="handleSuggestionClick"
-        >
+        <li v-for="(category, index) in matchedCategories" :key="category.id"
+          :class="{ suggestion__element: true, active: index === activeChoice.key }"
+          @mouseover="() => setActiveChoice(index, category)" @click="() => handleSuggestionClick(index, category)">
           {{ category.fullName.toLowerCase() }}
         </li>
       </ul>
@@ -176,6 +132,10 @@ function handleKeyDown(event: KeyboardEvent) {
 .suggestions li {
   padding: 8px;
   border-bottom: solid 1px rgba(255, 255, 255, 0.554);
+}
+
+.suggestion__element {
+  cursor: pointer;
 }
 
 .search-bar__label {

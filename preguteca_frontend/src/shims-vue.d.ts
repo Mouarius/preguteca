b1016112c313a77be691ee2354b5fe87c4685b3d
Marius Menault <mouarius@gmail.com>
2024-03-02 17:13:32 +0100

    change eslint config, fix shims vue

--- preguteca_frontend/src/shims-vue.d.ts
+++ preguteca_frontend/src/shims-vue.d.ts
@@ -1,5 +1,6 @@
 declare module "*.vue" {
   import { DefineComponent } from "vue";
-  const component: DefineComponent<{}, {}, any>;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const component: DefineComponent<object, object, any>;
   export default component;
 }
